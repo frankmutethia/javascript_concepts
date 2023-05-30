df03db4865cba831cd880ff30c0e1360aeb283bf
FrankMutethia <frankmutethia81@gmail.com>
2023-05-31 00:10:12 +0300

    spread operator

--- javascript/spread operator/spreadoperator.js
+++ javascript/spread operator/spreadoperator.js
@@ -32,3 +32,66 @@ const newsongs= {
 songs.push(newsongs);
 
 console.log(songs);
+
+//without push
+const songs1= [
+    {id1:1, name:"When the music stops"},
+    {id1:2, name:"The Plan"},
+    {id1:3, name:"I took a pill in Ibiza"},
+];
+
+const newsongs1= {
+    id:4,
+    name:"Money trees",
+    
+};
+const updatedsongs=[...songs,newsongs];
+// songs.push(newsongs);
+
+console.log(songs);
+console.log(updatedsongs);
+
+//another spread operator example
+const hits= {
+    title: "No Limit",
+    artist: "G-Eazy",
+    recordlabel: "Cash Money",
+ };
+ 
+ const newhit= {
+     ...hits,
+     otherartists:"Cardib,Rocky,French Montana",
+     
+ };
+ 
+ console.log(hits);
+ console.log(newhit);
+ 
+ 
+//spread operator within a nested object 
+const hits1= {
+    title: "No Limit",
+    artist: "G-Eazy",
+    recordlabel: "Cash Money",
+    relatedhits:{
+        title1:"Like That",
+        title2:"Sleepless",
+        title3:"Be Friends",
+        
+    }
+ };
+ 
+ const newhit1= {
+     ...hits1,
+     otherartists:"Cardib, Rocky, French Montana",
+       relatedhits:{
+        ...hits1.realatedhits,
+        title4:"Money Trees",
+ }
+ };
+ 
+ console.log(newhit1);
+ 
+ 
+ 
+ 
