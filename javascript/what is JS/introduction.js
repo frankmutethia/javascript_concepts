b3cf467f21d53b817c0ad9bb542f3d45d6803520
FrankMutethia <frankmutethia81@gmail.com>
2023-07-08 11:48:45 +0300

    async and defer

---
+++ javascript/what is JS/introduction.js
@@ -0,0 +1,20 @@
+//JavaScript is an object based scripting language, that is used to make websites dynamic and interactive.
+//Some of the practical uses of JavaScript include:
+//1. Building of mobile apps.
+//2. Interactive maps.
+//3. VR.
+//4. Booking systems.
+//5. Games.
+//6. Animations.
+//7. Data visualization.
+
+
+ //ADDING JS TO YOUR PROJECT:
+//Through the script tag and files.
+//The script tag should be added after the closing body tag.
+//Block and inline elements.
+
+
+// JAVASCRIPT : ASYNC AND DEFER
+//Async- allows your script to run as soon as it is loaded, without blocking other elements on that page.
+//Defer- means that your script will be executed after the page finishes loading.
