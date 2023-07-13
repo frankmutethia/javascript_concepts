495cc2f1ff87085e20eaf3b368429c5f749b3540
FrankMutethia <frankmutethia81@gmail.com>
2023-07-13 22:01:58 +0300

    number methods

---
+++ javascript/number methods/numbermethods.js
@@ -0,0 +1,17 @@
+
+//JAVASCRIPT NUMBER METHODS
+//Declaration and initialization of Pie to the nearest 5, in precision.
+// const PI= 3.141592653589793238;
+// let num= PI.toFixed(3);
+// num=PI.toPrecision(5);
+// console.log(num);
+
+//To exponential
+let milli= 100000;
+num = milli.toExponential();
+console.log(milli);
+
+let PIE= 3.141592653589793238;
+let num1= PIE.toFixed(3);
+num1= PIE.toPrecision(4);
+console.log(num1);
