b5d2a1d703a6c8321162bf1a8fc1b3d1dab9606f
FrankMutethia <frankmutethia81@gmail.com>
2023-07-22 23:56:06 +0300

    math object

---
+++ javascript/math object/mathobject.js
@@ -0,0 +1,23 @@
+
+//JAVASCRIPT MATH OBJECT
+const PI = Math.PI;
+console.log(PI.toFixed(2));
+
+let min = Math.min(1,2,57,1400,2100,7000);
+console.log(min);
+
+//power method
+let pow = Math.pow(5,3);
+console.log(pow);
+
+let random = Math.random().toFixed(1)*10;
+console.log(random);
+
+//random method
+let num100= Math.random().toFixed(1)*10;
+let num101= Math.random().toFixed(1)*10;
+let num102= Math.random().toFixed(1)*10;
+
+console.log(num100,num101,num102);
+let max= Math.max(num100,num101,num102);
+console.log(max);
--- learn.js
+++ learn.js
@@ -2,27 +2,6 @@
 
 
 
-//JAVASCRIPT MATH OBJECT
-const PI = Math.PI;
-console.log(PI.toFixed(2));
-
-let min = Math.max(1,2,57,1400,2100,7000);
-console.log(min);
-
-let pow = Math.pow(5,3);
-console.log(pow);
-
-let random = Math.random().toFixed(1)*10;
-console.log(random);
-
-let num100= Math.random().toFixed(1)*10;
-let num101= Math.random().toFixed(1)*10;
-let num102= Math.random().toFixed(1)*10;
-
-console.log(num100,num101,num102);
-let max= Math.max(num100,num101,num102);
-console.log(max);
-
 
 //JAVASCRIPT COMPARISON OPERATORS
 //Used to compare values and all comparison operators are usually boolean.
