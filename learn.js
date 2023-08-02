3f97f5b140f7c2cb886ea5a9088645e840fe919c
FrankMutethia <frankmutethia81@gmail.com>
2023-08-02 23:26:24 +0300

    ternary conditional operator

--- learn.js
+++ learn.js
@@ -1,27 +1,4 @@
 
-//JAVASCRIPT TERNARY OPERATOR
-//Ternary meaning- composed of 3 parts.
-//Provides us with an easier way to write a if else statement.
-
-let age = 18;
-if(age>=18){
-console.log('You are old enough to take shots.');
-}
-else{
-    console.log('Unafanya nini na mtoto?');
-
-}
-
-let age70 = 24;
-let message = age70 >=18  ? "You are old enough to take shots" : "Unafanya nini na mtoto?";
-console.log(message);
-
-
-let password20 = 'The Matrix';
-let message20 = (password20 === 'The Matrix') ? "Success":"Fail";
-console.log(message20);
-
-
 //JAVASCRIPT FOR LOOP
 //Loops allow performing of more tasks with less code.
 //Used to loop over a piece of code for several times. Composed of:
